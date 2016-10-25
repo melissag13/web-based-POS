dcb7ccf62f4ff42aa0439ddd77832d8feb4c0cb7
Kai <huikaizhong@yahoo.com>
2016-10-25 11:29:17 -0400

    update notes

---
+++ SEO notes.js
@@ -0,0 +1,16 @@
+Insights and Analytics:
+
+Compare stats from all your sales, explore detailed reports, and discover which products are most popular.
+
+Specialist support:
+
+Our dedicated POS team is on call 24/7 to make sure you have all the answers and advice you need.
+
+The world’s fastest growing commerce platform
+
+Tap a product to start building a cart
+Apply discounts or customize taxes
+Accept any form of payment
+Email customers customizable receipts
+
+The best part is you no longer have to spend time copying data from one platform to another. Shopify updates your inventory, orders, and customers in real-time so you don’t have to.
